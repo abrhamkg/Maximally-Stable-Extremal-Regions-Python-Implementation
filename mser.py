5ef7c2c86fb32d57d076830dfe8f010d8b18a0ba
Abrham Kahsay Gebreselasie <kgabrham@gmail.com>
2020-12-21 08:06:45 +0200

    Added main project files

---
+++ mser.py
@@ -0,0 +1,238 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 28 20:32:38 2020
+
+@author: abrih
+"""
+import numpy as np
+import cv2
+import bisect
+import os
+import matplotlib.pyplot as plt
+import copy
+import unionfind
+import logging
+import sys
+sys.setrecursionlimit(5000)
+DATA_DIR = 'data/mser/'
+
+
+def is_N4_adjacent(coord1, coord2):
+    """
+
+    """
+    #print(coord1, coord2)
+    return sum([abs(coord1[i] - coord2[i]) for i in range(len(coord1))]) == 1
+
+
+def find_neighbors(id, shape):
+    """
+    Finds the coordinates of neighbors of a given pixel.
+    """
+    return [id + 1, id - 1, id - shape[1], id + shape[1]]
+
+
+def coords2id(coords, shape):
+    return coords[0] * shape[1] + coords[1]
+
+
+def id2coords(pixid, shape):
+    return [pixid // shape[0], pixid % shape[0]]
+
+
+class MSER:
+    def __init__(self, num_thresh=5, max_area=20000, min_area=40, max_var=0.25,
+                 delta=2):
+        self.num_thresh = num_thresh
+        self.max_area = max_area
+        self.min_area = min_area
+        self.max_var = max_var
+        self.delta = 2
+
+        if self.num_thresh <= 2 * self.delta:
+            raise ValueError(f'The number of thresholds must at least \
+                             be 2 * delta + 1')
+
+    def detect(self, image):
+        if image.ndim > 2:
+            raise TypeError(f'A grayscale image expected but image had \
+                            {image.ndim} channels')
+
+        image_shape = image.shape
+        sorting_indices = np.argsort(image, axis=None)
+        row_indices, column_indices = np.unravel_index(sorting_indices,
+                                                       image_shape)
+        sorted_coordinates = np.array(list(zip(row_indices, column_indices)))
+
+        threshold_list = np.linspace(0, 255, self.num_thresh, endpoint=True)
+
+        output_images = np.zeros((self.num_thresh, image_shape[0], image_shape[1]), dtype=np.uint8)
+
+        sorted_intensity = image[row_indices, column_indices]
+
+        # connected component history
+        UF = unionfind.UnionFind(image.size)
+        for i, threshold in enumerate(threshold_list):
+            # Find the index at which intensity > thershold
+            end_index = bisect.bisect(sorted_intensity, threshold)
+            if end_index >= image.size:
+                continue
+            # print(start_index, end_index)
+            # output_images[i:, row_indices[start_index:end_index], column_indices[start_index:end_index]] = 255
+            output_images[i, row_indices[end_index:], column_indices[end_index:]] = 255
+            ids = np.apply_along_axis(coords2id, 1, sorted_coordinates[end_index:, :], image_shape)
+            neighbors = np.apply_along_axis(find_neighbors, 0, ids, image_shape).T
+            # print(len(ids))
+            # print(neighbors)
+            for j, pix_neighbors in enumerate(neighbors):
+                #print(pix_neighbors)
+                for neighbor in pix_neighbors:
+                    if neighbor < 0 or neighbor > image.size:
+                        continue
+
+                    pix = ids[j]
+                    if neighbor in ids:
+                        # print(f"Merging {neighbor} and {j}")
+                        UF.union(neighbor, pix)
+
+            # im = output_images[i]
+            # plt.imshow(im, cmap='gray')
+            # plt.figure()
+            # im = im.flatten()
+            # toplevel = UF.get_top_level_history()
+            # # print(len(toplevel), UF.count())
+            # for comp_history in toplevel:
+            #     if comp_history.size == 1:
+            #         continue
+            #     members = np.array(list(comp_history.members))
+            #     im[members] = np.random.randint(0, 256)
+            # im = im.reshape(image_shape)
+            # plt.imshow(im, cmap='gray', vmin=0, vmax=255)
+            # plt.title(str(i + 1))
+            # plt.figure()
+
+        all_history = UF.get_top_level_history()
+        # print(all_history)
+        history = filter(self.is_possibly_MSER, all_history)
+        msers = []
+        for parent_comp in history:
+            ph = [parent_comp.size, parent_comp.size]
+            pq = [float('inf'), float('inf')]
+            MSER.find_msers(parent_comp, msers, ph, pq, self.max_area, self.min_area, self.max_var)
+        #start_index = end_index
+        return output_images, msers
+
+    @staticmethod
+    def find_msers(parent_comp, msers, parent_sizes, parent_q, max_area=2000, min_area=2, max_var=0.25):
+        if parent_comp is None:
+            return [0] * len(parent_sizes)
+
+        q = (parent_sizes[-1] - parent_comp.children_sizes[0]) / parent_comp.size
+        ph = copy.copy(parent_sizes)
+        pq = copy.copy(parent_q)
+        ph = [parent_comp.size] + ph[:-1]
+        pq = [q] + pq[:-1]
+        # The Union-Find algorithm always puts the larger componenet on the left
+        child_q = MSER.find_msers(parent_comp.left, msers, ph, pq, max_var)
+        MSER.find_msers(parent_comp.right, msers, ph, pq, max_var)
+
+        all_q = child_q + [q] + parent_q
+        var = (parent_comp.size - parent_comp.children_sizes[-1])/(parent_comp.size)
+        print(var)
+        if min(all_q) == q and var < max_var and (min_area < parent_comp.size < max_area):
+            msers.append(parent_comp)
+        return child_q[1:] + [q]
+
+    def is_possibly_MSER(self, component):
+        return self.min_area < component.size < self.max_area
+
+    def detect2(self, image):
+        if image.ndim > 2:
+            raise TypeError(f'A grayscale image expected but image had \
+                            {image.ndim} channels')
+
+        image_shape = image.shape
+        sorting_indices = np.argsort(image, axis=None)
+        row_indices, column_indices = np.unravel_index(sorting_indices,
+                                                       image_shape)
+        sorting_coords = np.array(list(zip(row_indices, column_indices)))
+        # print(_coords.shape, pixel_coords.dtype, pixel_coords[:5])
+
+        output_images = np.zeros((self.num_thresh, image_shape[0],
+                                  image_shape[1]), dtype=np.uint8)
+        threshold_list = np.linspace(0, 255, self.num_thresh, endpoint=True)
+        sorted_intensity = image[row_indices, column_indices]
+
+        parent_list = -1 * np.ones_like(sorting_indices)
+        start_index = 0
+
+        for i, threshold in enumerate(threshold_list):
+            end_index = bisect.bisect(sorted_intensity, threshold)
+
+            pixel_positions = sorting_indices[start_index:end_index]
+            unmerged_pixels = copy.deepcopy(pixel_positions)
+            pixel_coords = sorting_coords[start_index:end_index]
+
+            for position, pixel in zip(pixel_positions, pixel_coords):
+                neighbors = np.apply_along_axis(is_N4_adjacent, 1,
+                                                sorting_coords, pixel)
+
+                added_to_existing = np.logical_and(neighbors,
+                                                   parent_list != -1)
+
+                # print(f"Added to existing {np.sum(added_to_existing)}")
+                if np.sum(added_to_existing):
+                    neighbor_parents = parent_list[added_to_existing]
+                    #print(neighbor_parents)
+                    parent_list[added_to_existing] = position
+                    for neighbor in neighbor_parents:
+                        parent_list[parent_list == neighbor] = position
+                        print(np.sum(parent_list == neighbor))
+                else:
+                    parent_list[position] = position
+                # print(neighbors) if np.sum(neighbors) > 0 else np.__version__
+                #print(np.unique(parent_list))
+                #print(f"We now have {len(np.unique(parent_list)) - 1} \
+                #componenets")
+            
+            components = np.unique(parent_list)
+            ncomponents = len(components)
+            color = {c:int(255.*i/ncomponents) for i, c in enumerate(components)}
+            for val, c in color.items():
+                parent_indices = np.nonzero(parent_list == val)
+                image_indices = sorting_indices[parent_indices]
+                
+                r_idx, col_idx = np.unravel_index(image_indices,
+                                                  output_images[i].shape)
+                #print(r_idx, col_idx)
+                output_images[i, r_idx, col_idx]  = c
+                #print(np.unique(output_images[i]))
+        return output_images
+
+
+if __name__ == '__main__':
+    # from scipy.ndimage import gaussian_filter
+    # image = np.zeros((50, 50))
+    # length = 5
+    # weight = np.linspace(0.1, 1, length).tolist() + np.linspace(1, 0.1, length).tolist()
+    # weight = np.repeat(np.array(weight), 2 * length).reshape((2*length, 2*length))
+    # print(weight.shape)
+    # image[10:20, 10:20] = 255
+    # image[30:40, 30:40] = 255
+    # image = gaussian_filter(image, sigma=2)
+    #
+    # cv2.imwrite(DATA_DIR + 'test.png', image)
+    im = cv2.imread(os.path.join(DATA_DIR, 'cameraman.png'))
+    im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+
+    mser_detector = MSER()
+    plt.imshow(im)
+    plt.figure()
+    output, history = mser_detector.detect(im)
+    print(sorted(history, key=lambda g:g.size, reverse=True))
+    # for i, im in enumerate(output):
+    #     plt.imshow(im, cmap='gray', vmin=0, vmax=255)
+    #     plt.title(str(i + 1))
+    #     plt.figure()
+
+    plt.show()
---
+++ unionfind.py
@@ -0,0 +1,132 @@
+import numpy as np
+
+
+class CompHistory(object):
+    instances = []
+    count = 0
+
+    def __init__(self, size=1, members=None, delta=2, children_sizes=None, parent_sizes=None):
+        if members is None:
+            members = set()
+
+        if children_sizes is None:
+            self.children_sizes = [0] * delta
+        else:
+            assert len(children_sizes) == delta
+            self.children_sizes = children_sizes
+
+        if parent_sizes is None:
+            self.parent_sizes = [0] * delta
+        else:
+            assert len(parent_sizes) == delta
+            self.parent_sizes = parent_sizes
+
+        self.delta = delta
+        self.size = size
+        self.name = CompHistory.count
+        CompHistory.instances.append(self)
+        self.toplevel = True
+        self.parent = None
+        self.right = None
+        self.left = None
+        self.members = members
+
+    def set_left(self, other):
+        self.left = other
+
+    def set_right(self, other):
+        self.right = other
+
+    def __add__(self, other):
+        size = self.size + other.size
+        self.toplevel = False
+        other.toplevel = False
+        operands = [self, other]
+        max_size, idx = max((self.size, 0), (other.size, 1))
+        larger = operands[idx]
+        children_sizes = [larger.children_sizes[-1], larger.size]
+
+        members = self.members.union(other.members)
+        history = CompHistory(size, members=members, children_sizes=children_sizes)
+        history.left = self
+        history.right = other
+        return history
+
+    def __str__(self):
+        return "CompHistory(size={}, toplevel={})".format(self.size, self.toplevel)
+
+    def __repr__(self):
+        return "CompHistory(size={}, toplevel={})".format(self.size, self.toplevel)
+
+
+class UnionFind(object):
+    def __init__(self, num_sites, delta=2):
+        self.__delta = delta
+        self.__N = num_sites
+        self.__componenet = np.arange(num_sites) # [-1] * self.__N #
+        self.__count = num_sites
+        self.__weight = np.ones_like(self.__componenet)
+        self.__history = {i:CompHistory(size=1, members={i,}) for i in range(num_sites)}
+
+    def add_history(self, id_list):
+        for site_id in id_list:
+            self.__history[site_id] = CompHistory()
+
+    def find(self, p):
+        parent = self.__componenet[p]
+        while parent != p:
+            # Path compression
+            self.__componenet[p] = parent
+            p = parent
+            parent = self.__componenet[p]
+        return parent
+
+    def union(self, p, q):
+        p_root = self.find(p)
+        q_root = self.find(q)
+        if p_root == q_root:
+            return
+        if self.__weight[p_root] > self.__weight[q_root]:
+            p_root, q_root = q_root, p_root
+
+        self.__componenet[p_root] = q_root
+        self.__weight[q_root] += self.__weight[p_root]
+        self.__history[q_root] = self.__history[q_root] + self.__history[p_root]
+        self.__weight[p_root] = 0
+        self.__count -= 1
+
+    def connected(self, p, q):
+        return self.find(p) == self.find(q)
+
+    def count(self):
+        return self.__count
+
+    def componenets(self):
+        pass#all_components
+
+    def get_top_level_history(self):
+        return [history for history in self.__history.values() if history.toplevel]
+
+if __name__ == '__main__':
+    with open('data/uf/input.txt') as f:
+        lines = f.readlines()
+        num_sites, lines = int(lines[0].strip("\n")), lines[1:]
+        a = UnionFind(num_sites)
+        for line in lines:
+            p, q = line.strip("\n").split()
+            p, q = int(p), int(q)
+            a.union(p, q)
+    print(a.count())
+    history = a.get_top_level_history()
+    print(history)
+    b = history[0]
+
+    # print(a.count())
+    # a.union(0, 1)
+    # print(a.count(), a.find(0), a.find(1))
+    # a.union(1, 0)
+    # print(a.count(), a.find(0), a.find(1))
+    # a.union(2, 0)
+    # print(a.count(), a.find(0), a.find(1))
+
+
